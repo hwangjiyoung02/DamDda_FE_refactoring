143c7c85293d73db6046c06ba622e59d510d14f4
Jiyoung Hwang <104750081+hwangjiyoung02@users.noreply.github.com>
2024-10-22 20:30:12 +0900

    Update ProjectTitle.jsx

--- src/components/detail/ProjectTitle.jsx
+++ src/components/detail/ProjectTitle.jsx
@@ -1,15 +1,17 @@
 export const ProjectTitle = ({ projectTitle }) => {
-  return (
-    <div className="container">
-      <div style={{ paddingTop: "20px", textAlign: "center" }}>
-        <div className="project-info">
-          <div className="category">{projectTitle.category}</div>
-          <div className="presenter">{projectTitle.nickName}</div>
+    return (
+        <div className="container">
+            <div style={{ paddingTop: '20px', textAlign: 'center' }}>
+                <div className="project-info">
+                    <div className="category">{projectTitle.category}</div>
+                    <div className="presenter">{projectTitle.nickName}</div>
 
-          <h1 className="project-title">{projectTitle.title}</h1>
-          <p className="project-description">{projectTitle.description}</p>
+                    <h1 className="project-title" style={{ fontWeight: 'bold' }}>
+                        {projectTitle.title}
+                    </h1>
+                    <p className="project-description">{projectTitle.description}</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
