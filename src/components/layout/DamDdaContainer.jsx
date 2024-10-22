9f9b2a23480912153bbdf06c5362aa53a67c3719
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2024-10-22 20:57:50 +0900

    Update DamDdaContainer.jsx

--- src/components/layout/DamDdaContainer.jsx
+++ src/components/layout/DamDdaContainer.jsx
@@ -1,6 +1,7 @@
 import { Header } from "components/layout/Header";
 import { Footer } from "components/layout/Footer";
 import "components/layout/css/DamDdaContainer.module.css"; // 오타 수정
+import TranslatePage from "components/common/TranslatePage";
 import React, { useState } from "react";
 
 export function Layout({ children }) {
@@ -11,6 +12,7 @@ export function Layout({ children }) {
         {children} {/* 페이지 내용이 여기에 들어감 */}
       </div>
       <Footer />
+      <TranslatePage /> {/* 모든 페이지에서 번역 기능을 사용하도록 추가 */}
     </>
   );
 }
